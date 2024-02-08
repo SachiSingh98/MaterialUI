2b0f635eb67af49a008a9b54824c174d9fc4c17f
Sachin <ss0985211@gmail.com>
2024-02-08 16:43:58 +0530

    We have added the checkbox field into the form

--- src/Topics/FormFields/FormFields.js
+++ src/Topics/FormFields/FormFields.js
@@ -1,36 +1,83 @@
-import React, { useState } from 'react'
-import {Button, TextField, Typography} from '@mui/material'
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 
 export default function FormFields() {
-    const initialState = {
-        name:"",
-        email:"",
-        password:""
-    }
-    const [value , setValue] = useState(initialState)
-    const [data , setdata] = useState("No Data")
-    const handleOnChange = (e)=>{
-        setValue({...value ,[e.target.name]:e.target.value})
-    }
 
+  const [value, setValue] = useState({name: "",
+  email: "",
+  password: "",
+  terms:false
+});
+  const [data, setdata] = useState("No Data");
 
+  const handleOnChange = (e) => {
+    setValue({...value , [e.target.name]:e.target.value})
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setdata(value);
+    console.log(value)
+  };
 
   return (
     <>
-        <TextField name='name' type='text' onChange={handleOnChange} variant='filled' placeholder='Name' sx={{margin:3}} /> <br />
-        <TextField name='email' type='email' onChange={handleOnChange} variant='outlined' placeholder='Email' sx={{margin:3}} /> <br />
-        <TextField name='password' type='password' onChange={handleOnChange} variant='standard' placeholder='Password' sx={{margin:3}} /> <br />
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <TextField
+          name="name"
+          type="text"
+          onChange={handleOnChange}
+          variant="filled"
+          placeholder="Name"
+          sx={{ margin: 3 }}
+        />
+        <TextField
+          name="email"
+          type="email"
+          onChange={handleOnChange}
+          variant="outlined"
+          placeholder="Email"
+          sx={{ margin: 3 }}
+        />
+        <TextField
+          name="password"
+          type="password"
+          onChange={handleOnChange}
+          variant="standard"
+          placeholder="Password"
+          sx={{ margin: 3 }}
+        />
 
+        {/* Check Boxes */}
+        <FormGroup>
+          <FormControlLabel  sx={{ margin: "5px" }} control={<Checkbox  name="terms" onChange={(e) => {
+          setValue({ ...value, terms: e.target.checked });
+        }} />} label="Agree to terms" name="termsandcondition" />
+        </FormGroup>
 
-        <Button sx={{margin:"5px"}} onClick={()=>setdata(value)} variant='contained'>
-            Get Data
+        <Button
+          type="Submit"
+          sx={{ margin: "5px" }}
+          onClick={handleOnSubmit}
+          variant="contained"
+        >
+          Get Data
         </Button>
+      </form>
 
-        <Typography margin={3} >
-            {data.name} <br/>
-            {data.email} <br/>
-            {data.password} <br/>
-        </Typography>
+      <Typography margin={3}>
+        {data.name} <br />
+        {data.email} <br />
+        {data.password} <br />
+        {data.terms ? "Agree" : null } 
+      </Typography>
     </>
-  )
+  );
 }
