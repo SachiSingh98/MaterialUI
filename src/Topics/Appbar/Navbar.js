dbbd9669cc4a6666d379461252ddf3c54ac19c6f
Sachin <ss0985211@gmail.com>
2024-02-08 17:59:51 +0530

    Completed the tab and tabs

--- src/Topics/Appbar/Navbar.js
+++ src/Topics/Appbar/Navbar.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import {AppBar , Button, Toolbar, Typography} from '@mui/material'
+import React, { useState } from "react";
+import { Tabs, Tab, AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 export default function Navbar() {
-    // The app bar component is like nav tag in html and the toolbar helps to add the information in a systematic way to it can be in a flex manner
+  // The app bar component is like nav tag in html and the toolbar helps to add the information in a systematic way to it can be in a flex manner.
+  const [tabValue, setTableValue] = useState(0);
   return (
     <>
       <AppBar>
         <Toolbar>
-            <Typography variant='h5' >Logo</Typography>
-            <Button variant='contained' sx={{marginLeft:"auto"}} >Login</Button>
+          <Typography variant="h5">Logo</Typography>
+
+          {/* Tabs and Tab */}
+          <Tabs 
+          sx={{marginLeft:"auto"}}
+            value={tabValue}
+            indicatorColor="primary"
+            textColor="inherit"
+            onChange={(e, val) => setTableValue(val)}
+          >
+            <Tab label="First" />
+            <Tab label="Second" />
+            <Tab label="Third" />
+
+          </Tabs>
+
+          <Button variant="contained" sx={{ marginLeft: "auto" }}>
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </>
-  )
+  );
 }
