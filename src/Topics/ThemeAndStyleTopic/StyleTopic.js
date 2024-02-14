575152c7a5143b8e268b9f697d5bc755a67fdcf5
Sachin <ss0985211@gmail.com>
2024-02-14 13:47:16 +0530

    Completed the style and advance styling

--- src/Topics/ThemeAndStyleTopic/StyleTopic.js
+++ src/Topics/ThemeAndStyleTopic/StyleTopic.js
@@ -1,4 +1,4 @@
-import { Typography, styled } from '@mui/material'
+import { AppBar, Toolbar, Typography, styled } from '@mui/material'
 import React from 'react'
 
 export default function StyleTopic() {
@@ -10,13 +10,31 @@ export default function StyleTopic() {
         background:green
     }
     `
+
+    // Advance component
+    const NavBar = styled(AppBar)(({theme})=>({
+        background:"red",
+        [theme.breakpoints.down("sm")]:{
+            background:"green"
+        }
+    }))
   return (
 
     // In this we can add custom styling .
+
+
+    // In this we will learn about the differnce between sx and style prop.
+    // Style apply css properties while the sx provides the mui properties.
     <>
-      <CustomizedTypography>
+      <CustomizedTypography marginTop="100px" >
         Hello this is customize Typography
       </CustomizedTypography>
+
+      <NavBar>
+        <Toolbar>
+
+        </Toolbar>
+      </NavBar>
     </>
   )
 }
