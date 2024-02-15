f1710ef96ef9f453ce58169d8b331c3746e66f19
Sachin <ss0985211@gmail.com>
2024-02-15 10:45:14 +0530

    Completed the usemediaQuerry hook

--- src/Topics/ResponsiveStyling/ResponsiveStyling.js
+++ src/Topics/ResponsiveStyling/ResponsiveStyling.js
@@ -1,7 +1,11 @@
-import {Box, Typography } from '@mui/material'
+import {Box, Typography, useMediaQuery, useTheme } from '@mui/material'
 import React from 'react'
 
 export default function ResponsiveStyling() {
+  const theme = useTheme()
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"))
+  const isBetweenSmAndsxs = useMediaQuery(theme.breakpoints.between(200 , 400))
+
   return (
     <>
         <Typography fontSize={{xs:10 , sm:15 , md:20 , lg:25 , xl:30}} variant='h1'>
@@ -9,8 +13,13 @@ export default function ResponsiveStyling() {
         </Typography>
 
 
-        <Box width={{xl:800 , lg:600 , md:400 , sm:200 , xs:100}} height={{xl:800 , lg:600 , md:400 , sm:200 , xs:100}}  bgcolor="red">
+        {/* <Box width={{xl:800 , lg:600 , md:400 , sm:200 , xs:100}} height={{xl:800 , lg:600 , md:400 , sm:200 , xs:100}}  bgcolor="red"> </Box> */}
+
+        <Box>
+          <Typography>{isBelowMd ? "Below" : "Above"}</Typography>
+          <Typography>{isBetweenSmAndsxs ? "Between" : "Not between"}</Typography>
         </Box>
+
 
 
     </>
